7eabc1eabd546c00c13d033798155d30122816c3
ngzaharias <applzor@gmail.com>
2016-01-30 23:25:04 +1000

    Added a float lerp

---
+++ Assets/PlayMaker Custom/Actions/FloatLerp.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Logic)]
+	[Tooltip("Gets the value between float1 and float2 at the delta.")]
+	public class FloatLerp : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("Float 1.")]
+		public FsmFloat float1 = 0.0f;
+
+		[RequiredField]
+		[Tooltip("Float 2.")]
+		public FsmFloat float2 = 1.0f;
+
+		[RequiredField]
+		[Tooltip("Delta.")]
+		public FsmFloat delta = 0.0f;
+
+		[RequiredField]
+		[Tooltip("Delta.")]
+		public FsmFloat outValue = 0.0f;
+
+		[Tooltip("If enabled, it will update every frame.")]
+		public bool isUpdatedEveryFrame;
+
+		public override void Reset()
+		{
+			float1				= 0.0f;
+			float2				= 1.0f;
+			delta				= 0.0f;
+			outValue			= 0.0f;
+			isUpdatedEveryFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			if (isUpdatedEveryFrame == true)
+				return;
+
+			Lerp();
+		}
+
+		public override void OnUpdate()
+		{
+			if (isUpdatedEveryFrame == false)
+				return;
+
+			Lerp();
+		}
+
+		private void Lerp()
+		{
+			outValue.Value = Mathf.Lerp(float1.Value, float2.Value, delta.Value);
+		}
+	}
+}

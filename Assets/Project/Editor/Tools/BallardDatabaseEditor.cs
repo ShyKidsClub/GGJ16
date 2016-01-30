3cb2542f8c268c1376530a61e72cbda7fe3d9d68
ngzaharias <applzor@gmail.com>
2016-01-30 13:43:28 +1000

    Added a BallardDatabase.

---
+++ Assets/Project/Editor/Tools/BallardDatabaseEditor.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BallardDatabaseEditor : EditorWindow
+{
+	static private BallardDatabase target = null;
+	static private BallardDatabaseEditor window = null;
+
+	// Creation of the Asset
+	[MenuItem("Assets/Create/Ballard Database")]
+	public static void CreateAsset()
+	{
+		ScriptableObjectUtility.CreateAsset<BallardDatabase>();
+	}
+
+	// Selecting the Asset
+	[MenuItem("Window/Ballard Editor")]
+	static void ShowWindow()
+	{
+		window = (BallardDatabaseEditor)EditorWindow.GetWindow(typeof(BallardDatabaseEditor), true, "Ballard Editor");
+		target = (BallardDatabase)AssetDatabase.LoadAssetAtPath("Assets/Project/Resources/BallardDatabase.asset", typeof(ScriptableObject));
+	}
+
+	void OnGUI()
+	{
+		if (target == null)
+			ShowWindow();
+
+		EditorGUILayout.Separator();
+		EditorGUILayout.BeginHorizontal();
+		{
+			if (GUILayout.Button("Add Ballard"))
+			{
+				target.AddBallard(new BallardData());
+			}
+			if (GUILayout.Button("Remove Ballard"))
+			{
+				int index = target.m_ballards.Count - 1;
+				target.RemoveBallard(index);
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.Separator();
+
+		// iterate through all the ballards
+		for (int i = 0; i < target.m_ballards.Count; ++i)
+		{
+			OnGUI_Ballard(i, target.m_ballards[i]);
+		}
+	}
+
+	bool[] isBallardFoldedOut = new bool[256];
+	void OnGUI_Ballard(int index, BallardData ballard)
+	{
+		EditorGUILayout.BeginHorizontal();
+		{
+			isBallardFoldedOut[index] = EditorGUILayout.Foldout(isBallardFoldedOut[index], ballard.name);
+			if (GUILayout.Button(" + "
+				, EditorStyles.miniButtonLeft
+				, GUILayout.MaxWidth(32.0f)))
+			{
+				int position = target.MoveBallard(index, -1);
+				SwitchValue(ref isBallardFoldedOut[index], ref isBallardFoldedOut[position]);
+			}
+			if (GUILayout.Button(" - "
+				, EditorStyles.miniButtonRight
+				, GUILayout.MaxWidth(32.0f)))
+			{
+				int position = target.MoveBallard(index, 1);
+				SwitchValue(ref isBallardFoldedOut[index], ref isBallardFoldedOut[position]);
+			}
+			if (GUILayout.Button("Remove"
+				, EditorStyles.miniButtonRight
+				, GUILayout.MaxWidth(128.0f)))
+			{
+				target.RemoveBallard(index);
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+
+		if (isBallardFoldedOut[index] == false)
+			return;
+
+		++EditorGUI.indentLevel;
+		// Ballard Options
+		EditorGUILayout.BeginHorizontal();
+		{
+			GUILayout.Space((EditorGUI.indentLevel + 1) * 10.0f);
+			GUILayout.Label("Name: ", GUILayout.MaxWidth(64.0f));
+			ballard.name = EditorGUILayout.TextField(ballard.name);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		// Ballard Sequences
+		ballard.debug_isFoldedOut = EditorGUILayout.Foldout(ballard.debug_isFoldedOut, "Sequences");
+		if (ballard.debug_isFoldedOut == true)
+		{
+			++EditorGUI.indentLevel;
+			for (int i = 0; i < ballard.sequences.Count; ++i)
+			{
+				OnGUI_Sequence(ballard, ballard.sequences[i], i);
+			}
+			--EditorGUI.indentLevel;
+		}
+
+		--EditorGUI.indentLevel;
+		EditorGUILayout.Separator();
+		EditorGUILayout.Separator();
+		EditorGUILayout.Separator();
+	}
+
+	void OnGUI_Sequence(BallardData parent, SequenceData sequence, int index)
+	{
+		sequence.debug_isFoldedOut = EditorGUILayout.Foldout(sequence.debug_isFoldedOut, sequence.name);
+		if (sequence.debug_isFoldedOut == false)
+			return;
+
+		++EditorGUI.indentLevel;
+
+		EditorGUILayout.BeginHorizontal();
+
+			GUILayout.Space((EditorGUI.indentLevel+2) * 10.0f);
+			GUILayout.Label("Name: ", GUILayout.MaxWidth(64.0f));
+			sequence.name = EditorGUILayout.TextField(sequence.name);
+
+		EditorGUILayout.EndHorizontal();
+
+		for (int i = 0; i < sequence.chords.Count; ++i)
+		{
+			OnGUI_Chord(sequence, sequence.chords[i], i);
+		}
+
+		--EditorGUI.indentLevel;
+	}
+
+	void OnGUI_Chord(SequenceData parent, ChordData chord, int index)
+	{
+		string clipName = (chord.clip != null ? chord.clip.name : "Clip");
+		chord.debug_isFoldedOut = EditorGUILayout.Foldout(chord.debug_isFoldedOut, clipName);
+		if (chord.debug_isFoldedOut == false)
+			return;
+
+		++EditorGUI.indentLevel;
+
+		EditorGUILayout.BeginHorizontal();
+		{
+			GUILayout.Space((EditorGUI.indentLevel) * 10.0f);
+			chord.timing = EditorGUILayout.DoubleField(chord.timing);
+			chord.clip = (AudioClip)EditorGUILayout.ObjectField(chord.clip, typeof(AudioClip), false);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		--EditorGUI.indentLevel;
+	}
+
+	void SwitchValue(ref bool A, ref bool B)
+	{
+		bool temp = A;
+		A = B;
+		B = temp;
+	}
+}
---
+++ Assets/Project/Scripts/Tools/ScriptableObjects/BallardDatabase.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class BallardData
+{
+	[HideInInspector]
+	public bool debug_isFoldedOut = false;
+
+	public string name = "BALLARD OF AWESOME";
+	public double duration = 0.0f;
+	public List<SequenceData> sequences = new List<SequenceData>();
+
+	public BallardData()
+	{
+		sequences.Add(new SequenceData());
+		sequences.Add(new SequenceData());
+		sequences.Add(new SequenceData());
+		sequences.Add(new SequenceData());
+	}
+}
+
+[System.Serializable]
+public class SequenceData
+{
+	[HideInInspector]
+	public bool debug_isFoldedOut = false;
+
+	public string name = "SEQUENCE OF COOL";
+	public List<ChordData> chords = new List<ChordData>();
+
+	public SequenceData()
+	{
+		chords.Add(new ChordData());
+		chords.Add(new ChordData());
+		chords.Add(new ChordData());
+		chords.Add(new ChordData());
+	}
+}
+
+[System.Serializable]
+public class ChordData
+{
+	[HideInInspector]
+	public bool debug_isFoldedOut = false;
+
+	public double timing = 0.0f;
+	public AudioClip clip = null;
+}
+
+public class BallardDatabase : ScriptableObject
+{
+	private static BallardDatabase m_instance = null;
+	public static BallardDatabase Instance
+	{
+		get
+		{
+			if (m_instance == null)
+				m_instance = (BallardDatabase)Resources.Load("GameParameters", typeof(BallardDatabase));
+			if (m_instance == null)
+				Debug.LogError("BallardDatabase doesn't exsist yet! Use the Ballard Editor to create one. ->Window/Ballard Editor");
+			return m_instance;
+		}
+	}
+
+	public List<BallardData> m_ballards = new List<BallardData>();
+
+	public void AddBallard(BallardData ballard)
+	{
+		m_ballards.Add(ballard);
+	}
+
+	public int MoveBallard(int index, int amount)
+	{
+		if (index < 0 || index >= m_ballards.Count)
+			return index;
+
+		BallardData ballard = m_ballards[index];
+		return MoveBallard(ballard, amount);
+	}
+
+	public int MoveBallard(BallardData ballard, int amount)
+	{
+		int position = m_ballards.IndexOf(ballard) + amount;
+		position = Mathf.Min(m_ballards.Count - 1, position); // don't exceed count
+		position = Mathf.Max(0, position); // don't exceed 0
+
+		m_ballards.Remove(ballard);
+		m_ballards.Insert(position, ballard);
+		return position;
+	}
+
+	public void RemoveBallard(BallardData ballard)
+	{
+		m_ballards.Remove(ballard);
+	}
+
+	public void RemoveBallard(int index)
+	{
+		if (index < 0 || index >= m_ballards.Count)
+			return;
+
+		BallardData ballard = m_ballards[index];
+		RemoveBallard(ballard);
+	}
+}

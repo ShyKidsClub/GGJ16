6de1109802f2fff1625def7f89c4095a08ef514c
ngzaharias <applzor@gmail.com>
2016-01-31 15:59:02 +1000

    Added transitions for title and credits

---
+++ Assets/PlayMaker Custom/Actions/PlayMechanimTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Animator)]
+	[Tooltip("")]
+	public class PlayMechanimTrigger : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("")]
+		public FsmString trigger = "";
+
+		public override void OnEnter()
+		{
+			Animator controller = Owner.GetComponent<Animator>();
+			if (controller != null)
+			{
+				controller.SetTrigger(trigger.Value);
+			}
+		}
+	}
+}
